9e6c4e0e60f476fe3f76514367ec6c22f60c69da
FirstSage1 <voleynikov@yandex.ru>
2024-08-12 23:53:29 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+# Организация программы и методы строк
+# ===================================================
+my_string=input("Ваше имя? ")
+znakov="В вопросительной строке "+str(len(my_string))+" знака."
+new_string="Ваше имя? "
+print( my_string)
+print(znakov)
+print( my_string.upper())
+print( my_string.lower())
+print( new_string.replace(__old:=" ",__new:=""))
+print( my_string[0])
+print( my_string[-1])
